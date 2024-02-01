33c694a161d3a70629a07be04857e0b10c26fa51
SergioABH <bedoyasergio3@gmail.com>
2024-02-01 16:04:44 -0500

    Backend Project

---
+++ src/config.js
@@ -0,0 +1,19 @@
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const PORT = process.env.PORT;
+
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+const DB_PORT = process.env.DB_PORT;
+
+module.exports = {
+  FRONTEND_URL,
+  PORT,
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_PORT
+};
